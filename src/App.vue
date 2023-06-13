c32d42dbbe23e8c82d02e176bda7529a449d2d10
marianitadileo <mdileo36@gmail.com>
2023-06-13 17:59:41 +0200

    cards projects

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script>
-export default {
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        this.projects = resp.data.results;
+      })
+    }
+  },
+  components: { ProjectCard }
 }
 </script>
 
 <template>
-  <h1>hello</h1>
+  <div class="container">
+    <h2 class="text-center">Tutti i post</h2>
+    <div class="row row-cols-3 g-2 mt-2">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name:'ProjectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card border-info mb-3 h-100" style="max-width: 21rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
